b25897bcd5b997d8aca4b44c03504848f0119056
Juan Carlos Ballesteros <jcballesteros@localidata.com>
2019-11-06 14:29:52 +0100

    VirtuosoSync: Para la carga de Virtuoso con los datos obtenidos en el proceso bigFiles_CA

---
+++ loaders_IAM/VirtuosoSync_CA/sync/src/main/java/semantic/sync/util/Constants.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package semantic.sync.util;
+
+/**
+ * @author cmart
+ *
+ */
+public class Constants {
+	
+	
+	// Configuracion properties
+	public static final String FIELD_CATALOG_URL = "catalogURL";
+	public static final String FIELD_HOST = "host";
+	public static final String FIELD_USER = "user";	
+	public static final String FIELD_PASSWORD = "password";
+	public static final String FIELD_VIRTUOSO_USER = "virtuosoUser";
+	public static final String FIELD_VIRTUOSO_PASSWORD ="virtuosoPassword";
+	public static final String FIELD_VIRTUOSO_PORT = "virtuosoPort";
+	public static final String FIELD_VIRTUOSO_PATH_ISQL = "virtuosoPathToIsql";
+	public static final String FIELD_VIRTUOSO_ELDA_URI = "eldaURI";
+	public static final String FIELD_VIRTUOSO_FORMAT_FILE = "formatFile";
+	public static final String FIELD_GRAPH_URI = "graphURI";
+
+
+	//Etiquetas para cambiar [GRAPH_URI] de commands
+	public static final String ETQ_COMMAND_GRAPH_URI = "[GRAPH_URI]";
+}
