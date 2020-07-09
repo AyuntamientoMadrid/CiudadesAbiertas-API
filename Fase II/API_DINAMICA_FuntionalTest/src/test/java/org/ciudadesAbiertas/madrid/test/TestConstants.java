b842d8dd2ed30d95e55b22d696a3912baa740418
juan_carlos_ballesteros <jcballesteros@localidata.com>
2020-07-09 14:42:47 +0200

    Fase II: Tests funcionales para API Dinamica

---
+++ Fase II/API_DINAMICA_FuntionalTest/src/test/java/org/ciudadesAbiertas/madrid/test/TestConstants.java
@@ -0,0 +1,19 @@
+package org.ciudadesAbiertas.madrid.test;
+
+public class TestConstants {
+
+	public static String HOST = "http://127.0.0.1:8080";
+	public static String CONTEXT = "/dynamicAPI";
+	
+	public static String DRIVER_SELECTED = "firefox";
+
+	public static int TIMEOUT = 10;
+	
+	public static String PASSWORD_OK = "123456";
+	public static String USER_OK = "localidata";
+	
+	public static final String LOGIN_URL=HOST+CONTEXT+"/login";
+	
+	public static final String JSON_DEFINITION = "{{}";
+						
+}
