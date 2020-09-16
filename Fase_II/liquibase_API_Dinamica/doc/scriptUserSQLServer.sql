0f5b126bc41ac5717fa37334c7c67be028bafb71
juan_carlos_ballesteros <jcballesteros@localidata.com>
2020-09-16 10:10:01 +0200

    Directorio 'Fase II' renombrado

---
+++ Fase_II/liquibase_API_Dinamica/doc/scriptUserSQLServer.sql
@@ -0,0 +1,28 @@
+USE master
+GO
+DROP LOGIN apiDinamica;
+DROP DATABASE apiDinamica;
+CREATE DATABASE apiDinamica COLLATE SQL_Latin1_General_CP1_CI_AS;
+CREATE LOGIN apiDinamica WITH PASSWORD='Primera1';
+
+
+USE apiDinamica
+GO
+CREATE USER apiDinamica FOR LOGIN apiDinamica WITH DEFAULT_SCHEMA=dbo;
+ALTER LOGIN apiDinamica WITH DEFAULT_DATABASE = apiDinamica;
+
+GO
+EXEC sp_addrolemember 'db_ddladmin', 'apiDinamica';
+EXEC sp_addrolemember 'db_datareader', 'apiDinamica';
+EXEC sp_addrolemember 'db_datawriter', 'apiDinamica';
+
+GRANT EXECUTE TO apiDinamica WITH GRANT OPTION;
+
+
+USE apiDinamica
+GO
+CREATE SCHEMA apiDinamica AUTHORIZATION apiDinamica;
+
+
+GO
+ALTER USER apiDinamica WITH DEFAULT_SCHEMA=apiDinamica;
